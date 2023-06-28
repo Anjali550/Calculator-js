49aee90f1a2a28b9e8f08049f97338e5bddee94e
Anjali550 <sainianjali1097@gmail.com>
2023-06-28 18:00:30 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function del(){
+    let value=document.getElementById('screen').value;
+    document.getElementById('screen').value=value.substr(0,value.length-1);
+}
+
+let one=getElementById('one');
+let two=getElementById('two');
+let three=getElementById('three')
